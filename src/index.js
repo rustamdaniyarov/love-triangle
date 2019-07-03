87189db4062be8590389eb4e7ae71260031e5092
rustamdaniyarov <52420729+rustamdaniyarov@users.noreply.github.com>
2019-07-03 13:48:29 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var result = 0;
+   preferences.unshift(0);
+   
+   for(let a = 1; a < preferences.length; a++){
+       let b = preferences[a];
+       let c = preferences[b];
+       
+       result += a == preferences[c];
+   }
+   var randNum = result/3;
+   return randNum.toFixed();
+  };
